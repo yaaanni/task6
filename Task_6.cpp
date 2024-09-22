9943156f9e8589df7c3cea9a15693b65e964ab15
yaaanni <rw.yana.rw@gmail.com>
2024-09-22 19:34:33 +0300

    c

--- Task_6.cpp
+++ Task_6.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
 #include <cmath>
 int main() {
-    
+    double a,d,k,e,b,x,y,c;
+    int p;
+    e = 2,71828182845904;
+    std::cout << "Введите значение x: ";
+    std::cin >> x;
+    std::cout << "Введите значение p: ";
+    std::cin >> p;
+    std::cout << "Введите значение k: ";
+    std::cin >> k;
+    std::cout << "Введите значение d: ";
+    std::cin >> d;
+    std::cout << "Введите значение c: ";
+    std::cin >> c;
+    if (2*a*b==0) {
+        std::cout << "error";
+    } else if ( x > 1) {
+        std::cout << "error";
+    } else if ( -1 > x) {
+        std::cout << "error";
+    } else {
+        a = x+sin(p);
+    b = pow(e,k);
+    y = 1+pow(k,2)/(2*a*b)-b+d*c;
+    std::cout << y;
+    }
+    return 0;
 }
