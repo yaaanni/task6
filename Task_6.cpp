06cbaead582985aea9d5c985295d89a2c019c575
yaaanni <rw.yana.rw@gmail.com>
2024-09-22 21:26:40 +0300

    c

---
+++ Task_6.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+int main() {
+    double a,x,b,d,y,c,k;
+    std::cout << "Введите x: ";
+    std::cin >> x;
+    std::cout << "Введите d: ";
+    std::cin >> d;
+    std::cout << "Введите c: ";
+    std::cin >> c;
+    std::cout << "Введите k: ";
+    std::cin >> k;
+    if (x<=0) {
+        std::cout << "error";
+    } else if (k==0) {
+    std::cout << "error";
+    } else {
+        a = log10(x);
+        b = x + exp(d);
+        y = (a+b)-pow(c,2)/k;
+        std::cout << y;
+    }
+}
