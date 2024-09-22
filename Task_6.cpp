31596bb06ced344488230da42ed5435eb540f3b5
yaaanni <rw.yana.rw@gmail.com>
2024-09-22 20:46:14 +0300

    c

--- Task_6.cpp
+++ Task_6.cpp
@@ -1,26 +1,32 @@
 #include <iostream>
 #include <cmath>
 int main() {
-    double a,n,m,d,x,y,k;
-    std::cout << "Введите n: ";
-    std::cin >> n;
-    std::cout << "Введите m: ";
-    std::cin >> m;
+    double a,x,y,b,z,k,c,d,t;
     std::cout << "Введите x: ";
     std::cin >> x;
+    std::cout << "Введите y: ";
+    std::cin >> y;
+    std::cout << "Введите z: ";
+    std::cin >> z;
     std::cout << "Введите k: ";
     std::cin >> k;
+    std::cout << "Введите c: ";
+    std::cin >> c;
     std::cout << "Введите d: ";
     std::cin >> d;
-    if (a == 0) {
+    if (z < 0) {
         std::cout << "error";
-    } else if (cos(x) == 0) {
+    } else if (k-(c*d)==0){
         std::cout << "error";
-    } else {
-        a = fabs(n+m);
-        d = tan(x);
-        y = 1.29+k/a+ pow(d,2);
-        std::cout << y;
+    } else if (cos(x) > 1) {
+        std::cout << "error";}
+        else if (cos(x) < -1) {
+        std::cout << "error";}
+        else {
+        a = x-y;
+        b = sqrt(z);
+        t = cos(x)+pow(a,2)/(k-c*d)-b;
+        std::cout << t;
+        return 0;
     }
-
 }
